fd9c40b9a7633d80fc53e4901f5af944fd9f0a46
ggregoriodev <guilhermegregoriodossantos@gmail.com>
2026-08-01 19:10:14 -0300

    versao final

---
+++ order-service/src/test/java/com/ggregoriodev/order_service/controller/SdpControllerTest.java
@@ -0,0 +1,42 @@
+package com.ggregoriodev.order_service.controller;
+
+import com.ggregoriodev.order_service.dto.PedidoDto;
+import com.ggregoriodev.order_service.entity.PedidoEntity;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.swing.text.html.parser.Entity;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ActiveProfiles("test")
+
+class SdpControllerTest {
+
+
+    EntityManager entityManager;
+
+
+    @Test
+    void listarPedidos() {
+    }
+
+    @Test
+    void buscarPedidoPorId() {
+    }
+
+    @Test
+    void adicionarPedido() {
+    }
+
+    @Test
+    void deletarPedido() {
+    }
+
+    @Test
+    void atualizarPedido() {
+    }
+
+
+}
